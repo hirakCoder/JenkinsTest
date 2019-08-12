d7d7e495436c6f0088558a3b0b09f0baeaf393db
hirak10 <hirak.b@hotmail.com>
2019-08-11 21:00:57 -0400

    AddingjenkinsTestFile

---
+++ TestCI/src/Testmessage.java
@@ -0,0 +1,24 @@
+
+public class Testmessage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("MainTest");
+		
+		printone();
+		printtwo();
+		
+	}
+	
+	public static void printone() {
+		System.out.println("SteponeDone");
+	}
+	
+	public static void printtwo() {
+		System.out.println("StepTwoDone");
+	}
+	
+
+}
